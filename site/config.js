02168b403d11c4d18d1d7699a8db17f4c59b7b1f
kkringlen <kkringlen@gmail.com>
2026-07-26 23:14:08 -0500

    Fix Supabase project URL

--- site/config.js
+++ site/config.js
@@ -1,5 +1,5 @@
 window.HILLSIDE_CONFIG = {
-  SUPABASE_URL: 'https://tclfxpeutarjrxdovqvh.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://tclfxpeutarjrxdovqvh.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_UUZc2PDqnU_YQ1FIgNpMSA_7oIn0CK1',
   SITE_NAME: 'Hillside Connect',
   DEFAULT_COUNTRY_CODE: '+1',
